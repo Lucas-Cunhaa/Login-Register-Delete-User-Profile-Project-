62332bf263c3a0dc9b574ce7df9fb54eabebc8d2
Lucas Cunha <lucasgalvao260@gmail.com>
2024-04-02 11:48:18 -0300

    Adding table user on database

--- sequelize.js
+++ sequelize.js
@@ -1,8 +1,9 @@
 const Sequelize = require("sequelize");
-
+const {DataTypes} = Sequelize
 const sequelize = new Sequelize("userapp", "root", "12345", {
   dialect: "mysql",
 });
+
 
 async function connectDatabase() {
   try {
@@ -13,4 +14,37 @@ async function connectDatabase() {
   }
 }
 
+const User = sequelize.define('user', { 
+  id : { 
+    type : DataTypes.INTEGER,
+    allowNull : false, 
+    autoIncrement : true, 
+    primaryKey: true
+   }, 
+  username: {
+    type: DataTypes.STRING,
+    allowNull : false,
+    
+  }, 
+  email : {
+    type: DataTypes.STRING,
+    allowNull : false,
+  } , 
+  password: {
+    type: DataTypes.STRING,
+    allowNull : false,
+  }
+});
+
+async function createTable () {
+  try {
+    await User.sync()
+    console.log('Table and model synced')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+
 connectDatabase()
+createTable()
